49716603a82b2c1f9ab8492971f62b04c1dac1eb
hotaco-56 <ng.aiden04@gmail.com>
2024-10-01 20:29:57 -0700

    added red and blue barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -24,62 +24,120 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     """ """
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
-    num_ml_delivered = 0
+    red_ml_delivered = green_ml_delivered = blue_ml_delivered = 0
     gold_spent = 0
-    for barrel in barrels_delivered:
-        num_ml_delivered += barrel.ml_per_barrel
-        gold_spent += barrel.price
 
+    for barrel in barrels_delivered:
+        if (barrel.potion_type[0] == 1): #red potion
+            red_ml_delivered = barrel.ml_per_barrel * barrel.quantity
+        elif (barrel.potion_type[1] == 1):
+            green_ml_delivered = barrel.ml_per_barrel * barrel.quantity
+        elif (barrel.potion_type[2] == 1):
+            blue_ml_delivered = barrel.ml_per_barrel * barrel.quantity
+        gold_spent += barrel.price * barrel.quantity
 
     with db.engine.begin() as connection:
         connection.execute(sqlalchemy.text(
                 f"""
                 UPDATE global_inventory
-                SET num_green_ml = num_green_ml + {num_ml_delivered},
+                SET num_green_ml = num_green_ml + {green_ml_delivered},
+                    num_red_ml = num_red_ml + {red_ml_delivered},
+                    num_blue_ml = num_blue_ml + {blue_ml_delivered},
                     gold = gold - {gold_spent}
                 """
         ))
 
+    print(f"GREEN ML DELIVERED:\n {green_ml_delivered}\n")
+    print(f"RED ML DELIVERED:\n {red_ml_delivered}\n")
+    print(f"BLUE ML DELIVERED:\n {blue_ml_delivered}\n")
+    print(f"GOLD SPENT:\n {gold_spent}\n")
+
     return "OK"
 
-small_green_barrel_quantity = 0
 
 # Gets called once a day
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
     print(wholesale_catalog)
-    barrels_ordered = 0
 
     with db.engine.begin() as connection:
         inventory = connection.execute(sqlalchemy.text(
             """
-            SELECT num_green_ml, num_green_potions, gold
+            SELECT num_green_ml, 
+                   num_green_potions, 
+                   gold,
+                   num_red_ml,
+                   num_red_potions,
+                   num_blue_ml,
+                   num_blue_potions
             FROM global_inventory
             """
         ))
-        inventory_list = inventory.first()
-        num_green_potions = inventory_list[1]
-        gold = inventory_list[2]
 
-        if (num_green_potions < 10):
-                for barrel in wholesale_catalog:
-                        for i in range(barrel.quantity):
-                                if (barrel.potion_type[1] == 1 and barrel.price <= gold):
-                                        gold -= barrel.price
-                                        barrels_ordered += 1
-         
-        if (barrels_ordered > 0):
-                return [
+        inventory_dict = inventory.mappings().first()
+
+        num_green_potions = inventory_dict['num_green_potions']
+        gold_available = inventory_dict['gold']
+        num_red_potions = inventory_dict['num_red_potions']
+        num_blue_potions = inventory_dict['num_blue_potions']
+
+        red_sku = green_sku = blue_sku = ""
+        red_barrels_ordered = green_barrels_ordered = blue_barrels_ordered = 0
+
+        barrels_receipt = []
+
+        for barrel in wholesale_catalog:
+            if (barrel.potion_type[0] == 1 and num_red_potions < 3): #red barrel
+                for i in range(barrel.quantity):
+                    if (barrel.price <= gold_available):
+                        gold_available -= barrel.price
+                        red_barrels_ordered += 1
+                        red_sku = barrel.sku
+            elif (barrel.potion_type[1] == 1 and num_green_potions < 3): #green barrel
+                for i in range(barrel.quantity):
+                    if (barrel.price <= gold_available):
+                        gold_available -= barrel.price
+                        green_barrels_ordered += 1
+                        green_sku = barrel.sku
+            elif (barrel.potion_type[2] == 1 and num_blue_potions < 3): #blue barrel
+                for i in range(barrel.quantity):
+                    if (barrel.price <= gold_available):
+                        gold_available -= barrel.price
+                        blue_barrels_ordered += 1
+                        blue_sku = barrel.sku
+
+        # Create receipt
+        if (red_barrels_ordered > 0):
+            barrels_receipt.append(
                 {
-                        "sku": "SMALL_GREEN_BARREL",
-                        "quantity": barrels_ordered,
+                    "sku:": red_sku,
+                    "quantity": red_barrels_ordered
                 }
-        ] 
+            )
+        if (green_barrels_ordered > 0):
+            barrels_receipt.append(
+                {
+                    "sku": green_sku,
+                    "quantity": green_barrels_ordered
+                }
+            )
+        if (blue_barrels_ordered > 0):
+            barrels_receipt.append(
+                {
+                    "sku": blue_sku,
+                    "quantity": blue_barrels_ordered
+                }
+            )
+         
+        if (len(barrels_receipt) > 0):
+                print(f"BARRELS ORDERED:\n {barrels_receipt}\n")
+                print(f"GOLD LEFT:\n {gold_available}\n")
+                return barrels_receipt
 
     return [
         {
             "sku": [],
-            "quantity": [],
+            "quantity": 0,
         }
     ]
