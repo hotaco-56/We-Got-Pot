b0009a1a432daefd070696ee614821e8aa9c829d
Aiden Ng <87673483+hotaco-56@users.noreply.github.com>
2024-10-07 09:34:25 -0700

    Update barrels.py

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -92,7 +92,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         red_barrels_ordered = green_barrels_ordered = blue_barrels_ordered = 0
 
         for barrel in wholesale_catalog:
-            if barrel.sku == "MINI_RED_BARREL":
+            if barrel.sku == "MEDIUM_RED_BARREL":
                 if gold_available >= barrel.price:
                     barrels_receipt.append(
                         {
