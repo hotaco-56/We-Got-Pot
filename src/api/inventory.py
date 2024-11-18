07ba57dadb6c1cd3c197a3d6d377a86efd535fd2
hotaco-56 <ng.aiden04@gmail.com>
2024-11-17 17:08:23 -0800

    capacitty plan

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -35,9 +35,30 @@ def get_capacity_plan():
     capacity unit costs 1000 gold.
     """
 
+    with db.engine.begin() as connection:
+        capacities = connection.execute(sqlalchemy.text(
+            """
+            SELECT ml_capacity, potion_capacity, gold
+            FROM global_inventory
+            """
+        )).first()
+
+    purchase_pot = False
+    purchase_ml = False
+    ml_cap = capacities.ml_capacity
+    potion_cap = capacities.potion_capacity
+    gold = capacities.gold
+
+    if potion_cap == 50 and gold > 2000:
+        purchase_pot = True
+    if  ml_cap == 10000 and gold > 5000:
+        purchase_ml = True
+    if ml_cap == 20000 and potion_cap == 100 and gold >= 15000:
+        purchase_pot = True
+        
     return {
-        "potion_capacity": 0,
-        "ml_capacity": 0
+        "potion_capacity": 1 if purchase_pot else 0,
+        "ml_capacity": 1 if purchase_ml else 0
         }
 
 class CapacityPurchase(BaseModel):
@@ -51,5 +72,46 @@ def deliver_capacity_plan(capacity_purchase : CapacityPurchase, order_id: int):
     Start with 1 capacity for 50 potions and 1 capacity for 10000 ml of potion. Each additional 
     capacity unit costs 1000 gold.
     """
+
+    with db.engine.begin() as connection:
+        ml_transaction = f"Purchased {capacity_purchase.ml_capacity} ml capacity"
+        potion_transaction = f"Purchased {capacity_purchase.potion_capacity} potion capacity"
+
+        connection.execute(sqlalchemy.text(
+            """
+            -- We put ml in barrels so this makes sense.
+            INSERT INTO barrel_transactions (sku, quantity, gold_spent, transaction)
+            VALUES ('ml_capacity', :ml_cap_purchased, :ml_gold_spent, :ml_transaction);
+
+            -- We put potions in barrels so this makes sense.
+            INSERT INTO barrel_transactions (sku, quantity, gold_spent, transaction)
+            VALUES ('potion_capacity', :potion_cap_purchased, :potion_gold_spent, :potion_transaction);
+
+            UPDATE global_inventory 
+            SET ml_capacity = ml_capacity + (10000*:ml_cap_purchased),
+                potion_capacity = potion_capacity + (50*:potion_cap_purchased),
+                gold = 100 + 
+                    (SELECT CASE WHEN EXISTS (SELECT 1 FROM barrel_transactions)
+                        THEN (SELECT SUM(gold_spent) FROM barrel_transactions)
+                        ELSE 0
+                        END
+                    ) +
+                    (SELECT CASE WHEN EXISTS (SELECT 1 FROM cart_items)
+                        THEN (SELECT SUM(gold) FROM cart_items)
+                        ELSE 0
+                        END
+                    ) 
+            
+            """
+        ),
+            {
+                'ml_cap_purchased': capacity_purchase.ml_capacity,
+                'potion_cap_purchased': capacity_purchase.potion_capacity,
+                'ml_gold_spent': capacity_purchase.ml_capacity * -1000,
+                'potion_gold_spent': capacity_purchase.potion_capacity * -1000,
+                'ml_transaction': ml_transaction,
+                'potion_transaction': potion_transaction
+            }
+        )
 
     return "OK"
